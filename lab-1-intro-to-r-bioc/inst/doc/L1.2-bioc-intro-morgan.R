42f472fcc0745f4e5c89fad5b603b73261b9d68b
Martin Morgan <martin.morgan@roswellpark.org>
2016-07-10 21:00:39 -0400

    add objectives / lessons learned to each section

--- lab-1-intro-to-r-bioc/inst/doc/L1.2-bioc-intro-morgan.R
+++ lab-1-intro-to-r-bioc/inst/doc/L1.2-bioc-intro-morgan.R
@@ -9,3 +9,36 @@ suppressPackageStartupMessages({
     library(GenomicRanges)
 })
 
+## ----setup-objects--------------------------------------------------------------------------------
+library(Biostrings)
+library(GenomicRanges)
+
+## ----Biostrings, message=FALSE--------------------------------------------------------------------
+library(Biostrings)                     # Biological sequences
+data(phiX174Phage)                      # sample data, see ?phiX174Phage
+phiX174Phage
+m <- consensusMatrix(phiX174Phage)[1:4,] # nucl. x position counts
+polymorphic <- which(colSums(m != 0) > 1)
+m[, polymorphic]
+
+## ----methods, eval=FALSE--------------------------------------------------------------------------
+#  methods(class=class(phiX174Phage))      # 'DNAStringSet' methods
+
+## ----phiX-----------------------------------------------------------------------------------------
+library(Biostrings)
+data(phiX174Phage)
+
+## ----consensusMatrix------------------------------------------------------------------------------
+m <- consensusMatrix(phiX174Phage)[1:4,]
+polymorphic <- which(colSums(m != 0) > 1)
+mapply(substr, polymorphic, polymorphic, MoreArgs=list(x=phiX174Phage))
+
+## ----require--------------------------------------------------------------------------------------
+library(GenomicRanges)
+
+## ----help, eval=FALSE-----------------------------------------------------------------------------
+#  help(package="GenomicRanges")
+#  vignette(package="GenomicRanges")
+#  vignette(package="GenomicRanges", "GenomicRangesHOWTOs")
+#  ?GRanges
+
